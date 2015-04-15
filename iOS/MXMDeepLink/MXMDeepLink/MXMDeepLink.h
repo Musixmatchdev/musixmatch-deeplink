575fff8f539d8f20cbe0a4ca55298eb51cf05c8b
Loreto Parisi <loretoparisi@gmail.com>
2015-04-15 17:24:25 +0200

    Added iOS Deeplink Helper

---
+++ iOS/MXMDeepLink/MXMDeepLink/MXMDeepLink.h
@@ -0,0 +1,14 @@
+//
+//  MXMDeepLink.h
+//  MXMDeepLink
+//
+//  Created by Loreto Parisi on 14/02/14.
+//  Copyright (c) 2014 Loreto Parisi. All rights reserved.
+//
+
+#ifndef MXMDeepLink_MXMDeepLink_h
+#define MXMDeepLink_MXMDeepLink_h
+
+#import <MXMDeepLink/MXMDeepLinkHelper.h>
+
+#endif
